2616213e5e5bafd0fc93e4a231fd36bd9a05e55f
DarKaRo <jalp_18@hotmail.com>
2021-08-10 21:18:04 -0400

    Refactored index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const TILES_SIZE = Math.round(canvas.getAttribute('width') / TILES_AMOUNT)
 const GAME_WIDTH = TILES_SIZE * TILES_AMOUNT;
 const GAME_HEIGHT = TILES_SIZE * TILES_AMOUNT;
 
-let thicc = 0.5;
+let thicc = 1;
 
 canvas.setAttribute('width', GAME_WIDTH);
 canvas.setAttribute('height', GAME_HEIGHT);
@@ -36,95 +36,68 @@ function ClearCanvas() {
             let color = "";
             let isBorder = false;
             let isCircle = false;
+            let isEmpty = false;
+            let currentSize = TILES_SIZE;
 
-            if (x == 0 || y == 0 || x == TILES_AMOUNT - 1 || y == TILES_AMOUNT - 1) {
-                ctx.fillStyle = "#b9fe55";
-                ctx.fillRect(xPos, yPos, TILES_SIZE, TILES_SIZE);
-            }
+            if (x == 0 || y == 0 || x == TILES_AMOUNT - 1 || y == TILES_AMOUNT - 1) color = "#b9fe55";
 
             else {
-                     
-                let half = TILES_AMOUNT/2;
-                
-                if(x>=half-1 && x<=half && y>=half-1 && y<=half){
-                    
-                    ctx.fillStyle = "#555";
-                    ctx.fillRect(xPos, yPos, TILES_SIZE, TILES_SIZE);
-                }
+                isCircle = true;
+                isBorder = true;
 
-                else if(x>=half-2 && x<=half+1 && y>=half-2 && y<=half+1){
-                    drawBorder(xPos, yPos, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2), thicc)
-                    if(x<half){
-                        if(y>=half) ctx.fillStyle="#1d7ce6";
-                        else ctx.fillStyle="yellow";
+                let half = TILES_AMOUNT / 2;
+
+                if (x >= half - 1 && x <= half && y >= half - 1 && y <= half) color = "#555";
+
+                else if (x >= half - 2 && x <= half + 1 && y >= half - 2 && y <= half + 1) {
+                    if (x < half) {
+                        if (y >= half) color = "#1d7ce6";
+                        else color = "yellow";
                     }
-                    else{
-                        if(y>=half) ctx.fillStyle="white";
-                        else ctx.fillStyle="red";
+                    else {
+                        if (y >= half) color = "white";
+                        else color = "red";
                     }
-                    ctx.fillRect(xPos + thicc, yPos + thicc, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2));
                 }
 
-                else{
-                    ctx.fillStyle = "#10b952";
-                    ctx.fillRect(xPos, yPos, TILES_SIZE, TILES_SIZE);
-                    
-                }
-                
-                
-                ctx.beginPath();
-                ctx.arc(xPos + TILES_SIZE / 2, yPos + TILES_SIZE / 2, TILES_SIZE / 3.5, 0 * Math.PI, 2 * Math.PI);
-                ctx.stroke();
-                
-
-                if (x == 1 || y == 1 || x == TILES_AMOUNT - 2 || y == TILES_AMOUNT - 2) {
-
+                else if (x == 1 || y == 1 || x == TILES_AMOUNT - 2 || y == TILES_AMOUNT - 2) {
                     if (x < 3) {
-
-                        if (y < 3) {
-                            drawBorder(xPos, yPos, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2), thicc)
-                            ctx.fillStyle = "#FF0";
-
-                        }
-
-                        if (y > TILES_AMOUNT - 4) {
-                            drawBorder(xPos, yPos, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2), thicc)
-                            ctx.fillStyle = "#1d7ce6";
-
-                        }
-
-                        ctx.fillRect(xPos + thicc, yPos + thicc, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2));
-                        ctx.beginPath();
-                        ctx.arc(xPos + TILES_SIZE / 2, yPos + TILES_SIZE / 2, TILES_SIZE / 3.5, 0 * Math.PI, 2 * Math.PI);
-                        ctx.stroke();
-
+                        if (y < 3) color = "#FF0";
+                        else if (y > TILES_AMOUNT - 4) color = "#1d7ce6";
+                        else isEmpty = true;
                     }
 
                     else if (x > TILES_AMOUNT - 4) {
-
-                        if (y > TILES_AMOUNT - 4) {
-
-                            drawBorder(xPos, yPos, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2), thicc)
-                            ctx.fillStyle = "#FFF";
-                            
-                        }
-
-                        else if(y<3){
-                            drawBorder(xPos, yPos, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2), thicc)
-                            ctx.fillStyle = "#F00";
-                        }
-
-                        ctx.fillRect(xPos + thicc, yPos + thicc, TILES_SIZE - (thicc * 2), TILES_SIZE - (thicc * 2));
-                            ctx.beginPath();
-                            ctx.arc(xPos + TILES_SIZE / 2, yPos + TILES_SIZE / 2, TILES_SIZE / 3.5, 0 * Math.PI, 2 * Math.PI);
-                            ctx.stroke();
+                        if (y > TILES_AMOUNT - 4) color = "#FFF";
+                        else if (y < 3) color = "#F00";
+                        else isEmpty = true;
                     }
 
-
+                    else isEmpty = true;
                 }
+                else isEmpty = true;
             }
 
+            if (isEmpty) {
+                color = "#10b952";
+                isBorder = false;
+            }
 
+            if (isBorder) {
+                currentSize = currentSize - (thicc * 2)
+                drawBorder(xPos, yPos, currentSize, currentSize, thicc);
+                xPos += thicc;
+                yPos += thicc;
+            }
+
+            ctx.fillStyle = color;
+            ctx.fillRect(xPos, yPos, currentSize, currentSize);
+
+            if (isCircle) {
+                ctx.beginPath();
+                ctx.arc((x * TILES_SIZE) + TILES_SIZE / 2,(y * TILES_SIZE) + TILES_SIZE / 2, TILES_SIZE / 3.5, 0 * Math.PI, 2 * Math.PI);
+                ctx.stroke();
+            }
         }
     }
 }
